fe70e1273128f1e734c6615c5cf0fd41bf78ee4a
niclas-lock <niclas.lock@axa.de>
2017-05-24 14:03:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from OpenSSL import SSL
 
 app = Flask(__name__)
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('https-private-key.pem')
-context.use_certificate_file('https-public-cert.pem')
 
 @app.route("/")
 def main():
 	return "Welcome!"
 
 if __name__ == "__main__":
+	context = ('https-public-cert.pem', 'https-private-key.pem')
 	app.run(host='0.0.0.0', port=8080,
 	       debug = False, ssl_context=context)
