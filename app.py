2b35705a14b5340e8f950c7921c4be81377861d2
niclaslock <niclaslock25@gmail.com>
2017-05-24 08:41:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from OpenSSL import SSL
+
 app = Flask(__name__)
+context = SSL.Context(SSL.SSLv23_METHOD)
+context.use_privatekey_file('yourserver.key')
+context.use_certificate_file('yourserver.crt')
 
 @app.route("/")
 def main():
 	return "Welcome!"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8080)
+	app.run(host='0.0.0.0', port=8080,
+	       debug = False/True, ssl_context=context)
