dc2e0b6ecb7099bd7ad98e8b02ba4b5577e5da30
niclas-lock <niclas.lock@axa.de>
2017-05-30 11:15:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def main():
 
 if __name__ == "__main__":	
 	context = ('https-public-cert.pem', 'https-private-key.pem')
-	app.run(host='0.0.0.0', port=8080,
+	application.run(host='0.0.0.0', port=8080,
 	       debug = False, ssl_context=context)
