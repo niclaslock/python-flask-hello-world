f6650edc0e0bd96fc8ae2194697a0556140171f7
niclas-lock <niclas.lock@axa.de>
2017-05-30 10:26:22 +0200

    Update and rename app.py to myproject.py

--- myproject.py
+++ myproject.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def main():
 	return "Welcome!"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	application.run(host='0.0.0.0')
 	
 #	context = ('https-public-cert.pem', 'https-private-key.pem')
 #	app.run(host='0.0.0.0', port=8080,
