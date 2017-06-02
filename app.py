2e45a5e9ddc40a0a45a839c24b258206ff0d2c2e
niclas-lock <niclas.lock@axa.de>
2017-06-02 10:05:43 +0200

    changed path to certificates

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def main():
 	return "Welcome!"
 
 if __name__ == "__main__":	
-	context = ('https-public-cert.pem', 'https-private-key.pem')
+	context = ('/secrets/https-private-key/https-public-cert.pem', '/secrets/https-private-key/https-private-key.pem')
 	application.run(host='0.0.0.0', port=8080,
 	       debug = False, ssl_context=context)
