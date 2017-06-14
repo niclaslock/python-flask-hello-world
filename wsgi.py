2db7ba34f0999898e7c861bd1b52d2b07c4647b6
niclas-lock <niclas.lock@axa.de>
2017-06-14 11:08:00 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,5 +7,4 @@ def main():
 	return "Welcome!"
 
 if __name__ == "__main__":	
-	context = ('/secrets/https-private-key/https-public-cert.pem', '/secrets/https-private-key/https-private-key.pem')
 	application.run()
