873191d16b5372e149e825f0490718aefd40bf85
niclas-lock <niclas.lock@axa.de>
2017-06-14 11:04:52 +0200

    Update and rename app.py to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,4 @@ def main():
 
 if __name__ == "__main__":	
 	context = ('/secrets/https-private-key/https-public-cert.pem', '/secrets/https-private-key/https-private-key.pem')
-	application.run(host='0.0.0.0', port=8080,
-	       debug = False, ssl_context=context)
+	application.run()
