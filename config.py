8510d1a8e5d9aa4cf8010c2d3c327274381b5185
niclas-lock <niclas.lock@axa.de>
2017-06-14 13:52:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-keyfile = "/secrets/https-private-key/https-private-key.pem"
-certfile = "/secrets/https-private-key/https-public-cert.pem"
+keyfile = /secrets/https-private-key/https-private-key.pem
+certfile = /secrets/https-private-key/https-public-cert.pem
